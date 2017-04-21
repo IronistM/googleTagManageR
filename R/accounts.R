95f9a8f66f57daeb53162241ab9eb3f166d2248b
ironistm <parzakonis.m@gmail.com>
2017-04-21 12:22:43 +0300

    Bulk upload

--- R/accounts.R
+++ R/accounts.R
@@ -1,4 +1,4 @@
-#' Get account list
+#' Get GTM account list
 #'
 #' @details
 #'
@@ -10,6 +10,22 @@
 get_accounts_list <- function() {
   url <-
     paste0("https://www.googleapis.com/tagmanager/v1/accounts/")
+  f <- googleAuthR::gar_api_generator(url, "GET", data_parse_function = function(x) x)
+  f()
+}
+#' Gets a GTM Account.
+#'
+#'
+#' Set \code{options(googleAuthR.scopes.selected = c(https://www.googleapis.com/auth/tagmanager.edit.containers, https://www.googleapis.com/auth/tagmanager.manage.accounts, https://www.googleapis.com/auth/tagmanager.readonly)}
+#' Then run \code{googleAuthR::gar_auth()} to authenticate.
+#' See \code{\link[googleAuthR]{gar_auth}} for details.
+#'
+#' @param accountId The GTM Account ID
+#' @importFrom googleAuthR gar_api_generator
+#' @export
+get_accounts <- function(accountId) {
+  url <- sprintf("https://www.googleapis.com/tagmanager/v1/accounts/%s", accountId)
+  # tagmanager.accounts.get
   f <- googleAuthR::gar_api_generator(url, "GET", data_parse_function = function(x) x)
   f()
 }
