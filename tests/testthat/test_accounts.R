d7631354a970d221c6dcc4ee85700f1d51362038
ironistm <parzakonis.m@gmail.com>
2017-05-01 19:11:48 +0300

    Added a gtm_trigger function

--- tests/testthat/test_accounts.R
+++ tests/testthat/test_accounts.R
@@ -1,2 +1,7 @@
 # library(googleTagManageR)
 context("Accounts fetching")
+
+test_that("List accounts", {
+  accounts <- gtm_accounts_list()
+  expect_s3_class(accounts, "data.frame")
+})
---
+++ tests/testthat/test_auth.R
@@ -0,0 +1,6 @@
+# context("Auth")
+#
+# test_that("Authenticate works", {
+#   auth <- googleTagManageR::gtm_auth()
+#   expect_s3_class(auth, "Token2.0")
+# })
