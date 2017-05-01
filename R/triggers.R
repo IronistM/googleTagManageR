2a451809eb1cbd2eff5ba67e966d548c46d73cd0
ironistm <parzakonis.m@gmail.com>
2017-05-01 19:07:15 +0300

    Changed get_ to to gtm_ for triggers list

--- R/triggers.R
+++ R/triggers.R
@@ -7,7 +7,35 @@
 #' @family get functions
 #' @export
 #'
-get_triggers_list <- function(accountId, containerId) {
+gtm_triggers_list <- function(accountId, containerId) {
+  url <-
+    paste0(
+      "https://www.googleapis.com/tagmanager/v1/accounts/",
+      accountId,
+      "/containers/",
+      containerId,
+      "/triggers"
+    )
+  # tagmanager.triggers.list
+  f <-
+    googleAuthR::gar_api_generator(
+      url,
+      "GET",
+      data_parse_function = function(x)
+        x
+    )
+  f()
+}
+#' Get a triggers details
+#'
+#' @details
+#'
+#' @return a daa frame of trigger detials
+#' @importFrom googleAuthR gar_api_generator
+#' @family get functions
+#' @export
+#'
+gtm_trigger <- function(accountId, containerId) {
   url <-
     paste0(
       "https://www.googleapis.com/tagmanager/v1/accounts/",
